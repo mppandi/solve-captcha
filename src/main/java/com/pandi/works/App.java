9fb76c30ee89bcf03682f2ce42bbd462417e8546
mppandi <pandi84.mca@gmail.com>
2020-09-18 16:18:08 +0530

    Update App.java
    comment added

--- src/main/java/com/pandi/works/App.java
+++ src/main/java/com/pandi/works/App.java
@@ -12,7 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Captcha Assignemnt!" );
         SpringApplication.run(App.class, args);
     }
 }
